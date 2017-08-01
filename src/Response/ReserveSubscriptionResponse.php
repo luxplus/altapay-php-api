4c2947156c06729fd12a0512e7c9c9352e4ea3ad
Nick Shawnley <shawnley@gmail.com>
2017-08-01 12:45:27 +0200

    Update ReserveSubscriptionResponse.php

--- src/Response/ReserveSubscriptionResponse.php
+++ src/Response/ReserveSubscriptionResponse.php
@@ -44,8 +44,6 @@ class ReserveSubscriptionResponse extends ChargeSubscriptionResponse
     public $Result;
 
     public $PreauthResult;
-  
-    public $MerchantErrorMessage
 
     /**
      * @var Transaction[]
@@ -59,16 +57,6 @@ class ReserveSubscriptionResponse extends ChargeSubscriptionResponse
     public function setPreauthAmount($PreauthAmount)
     {
         $this->PreauthAmount = (float) $PreauthAmount;
-        return $this;
-    }
-  
-    /**
-     * @param float MerchantErrorMessage
-     * @return ReserveSubscriptionResponse
-     */
-    public function setMerchantErrorMessage($MerchantErrorMessage)
-    {
-        $this->MerchantErrorMessage = $MerchantErrorMessage;
         return $this;
     }
 }
